848c71dffc6d42dfd7adf8dbafe41e6b560b7d05
Radica <z1176814811@gmail.com>
2025-05-16 20:52:26 +0800

    杂项

--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,4 @@
-#include "graphicslab/mainwindow.h"
-
-struct Color
-{
-    float r;
-    float g;
-    float b;
-    float a;
-    Color(float r, float g, float b, float a) : r(r), g(g), b(b), a(a) {}
-};
-
 int main()
 {
-    MainWindow window(640, 480, "My Window");
-
-    while (!window.shouldClose())
-    {
-        Color color(0, 0.5, 0.5, 1);
-        window.clear(color.r, color.g, color.b, color.a);
-        window.swapBuffers();
-        window.pollEvents();
-    }
-
     return 0;
 }
--- test_googletest/test.cpp
+++
@@ -1,9 +0,0 @@
-#include <gtest/gtest.h>
-
-int main(int argc, char** argv)
-{
-    // 初始化 Google Test 框架
-    ::testing::InitGoogleTest(&argc, argv);
-    // 运行所有测试用例
-    return RUN_ALL_TESTS();
-}
